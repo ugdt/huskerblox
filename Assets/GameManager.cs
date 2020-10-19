e73cb2bea53c0bb6e3799fd4ca94c1aa6df22467
Daniel Shchur <shchurgood@gmail.com>
2020-10-18 20:03:38 -0500

    GameScene: Added initial game manager scripts + time decrement

---
+++ Assets/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private uint time = 0;
+    
+    private UIManager uiManager = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiManager = GetComponent<UIManager>();
+        InvokeRepeating("DecrementTime", 0, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DecrementTime()
+    {
+        if (time == 0)
+            return;
+        time -= 1;
+        Debug.Log(time);
+        uiManager.UpdateTime(time);
+    }
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text timeField = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (timeField == null)
+            Debug.LogError("Time field is null!");
+    }
+
+    public void UpdateTime(uint seconds)
+    {
+        timeField.text = $"Time: {seconds}";
+    }
+
+
+}
